f4fccadac73f90fde33f5c32bac57752f0d418a0
Khushi Chhetri <khushi.chhetri@kriana.club>
2025-06-01 23:55:45 +0530

    About section

---
+++ Portfolio/src/components/Frameworks.jsx
@@ -0,0 +1,36 @@
+import { OrbitingCircles } from "./OrbitingCircles";
+
+export function Frameworks() {
+  const skills = [
+    "python",
+    "postgresql",
+    "css3",
+    "git",
+    "html5",
+    "javascript",
+    "reactNative",
+    "react",
+    "sqlite",
+    "tailwindcss",
+    "vitejs",
+    "nextjs",
+  ];
+  return (
+    <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
+      <OrbitingCircles iconSize={40}>
+        {skills.map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        ))}
+      </OrbitingCircles>
+      <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
+        {skills.reverse().map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        ))}
+      </OrbitingCircles>
+    </div>
+  );
+}
+
+const Icon = ({ src }) => (
+  <img src={src} className="duration-200 rounded-sm hover:scale-110" />
+);

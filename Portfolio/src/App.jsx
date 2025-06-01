8542148e80e1f4f322ff37586e536625404ac1c4
Khushi Chhetri <khushi.chhetri@kriana.club>
2025-06-01 14:23:28 +0530

    Added node modules and made 2 sections navbar and hero section

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,20 @@
+//rafce helps to give boiler plate for react
+import React from 'react'
+import Navbar from './sections/Navbar'
+
+const App = () => {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      <Navbar/>
+      {/* hero */}
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* testimonial */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  )
+}
+
+export default App
